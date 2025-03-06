3c0f57ed1f6b252b8dc653853ca3b2ee014eb110
Md Shaheer Ahmed <shaheerhasidea@gmail.com>
2025-03-06 19:09:30 +0530

    Committing all changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
   images: {
     unoptimized: true,
+    domains: ['cvly.me'],
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
